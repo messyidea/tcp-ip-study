7b329fc74af28f5da861bb2f0818eafcf37d7544
messyidea <the.mid.sea@gmail.com>
2015-03-03 17:47:11 +0800

    add signal and sigaction

---
+++ 信号signal/readme.md
@@ -0,0 +1,9 @@
+信号
+---
+
+#signal && sigaction
+
+sigaction更稳定，因为signal在不同操作系统中可能存在区别，但sigaction完全相同
+
+
+#利用sigaction去消灭僵尸进程
---
+++ 信号signal/signal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void timeout(int sig) {
+    if(sig == SIGALRM)
+        puts("time out");
+    alarm(2);
+}
+
+void keycontrol(int sig) {
+    if(sig == SIGINT) {
+        puts("ctrl + c pressed");
+    }
+}
+
+int main() {
+    int i;
+    signal(SIGALRM, timeout);
+    signal(SIGINT, keycontrol);
+    alarm(2);
+
+    for(i = 0;i < 3; i++) 
+    {
+        puts("wait");
+        sleep(100);
+    }
+    return 0;
+}
